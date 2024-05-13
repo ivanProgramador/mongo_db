cdb3b52066ca8f9872877ea106b505bcb9eefc57
Ivan <ivan-guita@hotmail.com>
2024-05-13 12:49:30 -0300

    Atualizar o criando_um_model.js
    corrigindo diretivas

--- anotacoes/criando_um_model.js
+++ anotacoes/criando_um_model.js
@@ -1,10 +1,10 @@
 /* Um model no mongo equivale a um objeto 
-   quase como o sequelize ´so que a sintaxe é mais simples 
-   No mongo a relção entre dados não se dá atraves de chaves estrangeiras 
-   é sim atraves de herança entre kos objetos 
+   quase como o sequelize só que a sintaxe é mais simples 
+   No mongo a relação entre dados não se dá através de chaves estrangeiras 
+   é sim atraves de herança entre os objetos 
 
-   por ecemplo: O model abaixo tem um campo chamdo resume que tem o author 
-   que pode ser dono de varios artigos e tambem tem o conteudo pois assim como 
+   por exemplo: O model abaixo tem um campo chamado resume que tem o author 
+   que pode ser dono de vários artigos e tambem tem o conteúdo pois assim como 
    um autor ppode ter varios artigos um conteudo tambem pode ter varios artigos
    falando sobre ele, então todos os objetos que compartilham essa semelhança 
    estaão de alguma forma ligados um a outro ´pe assim que se estabelece uma ligação 
